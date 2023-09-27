ddc92e777d0ac7a487c3bdd42937633afcc9ee9f
Grayson <glb002@mcdaniel.edu>
2023-09-27 16:20:26 -0400

    Add domain to config obj.

--- server.js
+++ server.js
@@ -9,17 +9,16 @@ const config = {
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   server: process.env.DB_SERVER, 
-  database: process.env.DB_NAME 
+  database: process.env.DB_NAME,
+  domain: proxy
 };
 
 app.get("/highScores", (req, res) => {
-  sql.proxy = proxy;
   sql.connect(config, function (err) {
     if (err) console.log(err);
 
     let request = new sql.Request();
-        
-    //request.proxy = proxy;
+
     request.query('[dbo].[p_Get_Labyrinth_HighScores]', function (err, recordset) {
       if (err) {
         console.log(err)
