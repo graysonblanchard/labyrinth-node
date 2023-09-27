fc025cdc5b0bf774d88068ff13206ad0fe7df3fc
Grayson <glb002@mcdaniel.edu>
2023-09-27 16:22:13 -0400

    Remove domain from config obj.

--- server.js
+++ server.js
@@ -9,8 +9,7 @@ const config = {
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   server: process.env.DB_SERVER, 
-  database: process.env.DB_NAME,
-  domain: proxy
+  database: process.env.DB_NAME
 };
 
 app.get("/highScores", (req, res) => {
