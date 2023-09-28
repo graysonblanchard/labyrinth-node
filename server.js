9a0f94f0a2389ff5c9e007a7e80a21a90028af69
Grayson <glb002@mcdaniel.edu>
2023-09-28 10:58:10 -0400

    Try sending individual params.

--- server.js
+++ server.js
@@ -44,7 +44,8 @@ app.post('/highScores', (req, res) => {
     
     // to-do: try sending separate params again ?
     request
-      .input('HighScoreJson', sql.VarChar(100), JSON.stringify(req.body))
+      .input('Name', sql.VarChar(50), req.body.name)
+      .input('Score', sql.VarChar(50), req.body.score)
       .execute('[dbo].[p_Manage_Labyrinth_HighScores]', function (err, recordset) {
         if (err) {
           console.log(err)
